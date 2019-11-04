2023c8c3872d61b4866929b7f921d09d07e329b7
Branislav Beno <brano.beno@gmail.com>
2019-11-05 00:19:41 +0100

    Backported configuration - part 2

--- src/test/java/com/benito/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
+++ src/test/java/com/benito/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
@@ -1,27 +1,12 @@
 package com.benito.cicdappliedtospringbootjavaapp;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import com.benito.cicdappliedtospringbootjavaapp.controller.HelloController;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
-
-public class CicdAppliedToSpringBootJavaAppApplicationTests {
-	
-	@Autowired
-	private HelloController helloController;
+class CicdAppliedToSpringBootJavaAppApplicationTests {
 
 	@Test
-	public void contextLoads() {
-		
-		assertEquals("Hello World from DZONE",helloController.home() );
+	void contextLoads() {
 	}
-
 }
