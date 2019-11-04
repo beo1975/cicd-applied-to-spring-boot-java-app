02e53d85c7f9d1182306b82682c005229b371acf
Branislav Beno <brano.beno@gmail.com>
2019-11-04 23:54:15 +0100

    Extended configuration - part 2

--- src/test/java/com/benito/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
+++ src/test/java/com/benito/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
@@ -1,13 +1,27 @@
 package com.benito.cicdappliedtospringbootjavaapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.benito.cicdappliedtospringbootjavaapp.controller.HelloController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class CicdAppliedToSpringBootJavaAppApplicationTests {
+
+public class CicdAppliedToSpringBootJavaAppApplicationTests {
+	
+	@Autowired
+	private HelloController helloController;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		
+		assertEquals("Hello World from DZONE",helloController.home() );
 	}
 
 }
